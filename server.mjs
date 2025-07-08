e0132adc0a61a02289bd1f49f76b0a6d9b1b11ba
megahomyak <g.megahomyak@gmail.com>
2025-07-08 18:41:50 +0500

    some stuff

--- server.mjs
+++ server.mjs
@@ -1,5 +1,31 @@
-listen(msg => {
-    msg = decrypt(msg, selfPrivkey);
-    assert(validSenders.contains(msg.sender)); // i may not need this in some network configurations
-    msg = unwrapSigned(msg); // checks for signature correctness: arg={ msg, signature, sender } -> assert stringifyStable({sender, msg}) == signature -> return msg;
+import stableStringify from "fast-json-stable-stringify";
+
+let wrapSigned = async (msg, sigPrivkey, sigPubkey) => {
+    let signature = sign(stableStringify({ msg, signer: sigPubkey }), sigPrivkey);
+    return { signer: sigPubkey, msg, signature };
+};
+
+let unwrapSigned = async msg => {
+    assert(getSignatureValidity(stableStringify({ msg: msg.msg, signer: msg.signer }), msg.signer, msg.signature));
+    return msg.msg;
+};
+
+let broadcast = async receiver => {
+
+};
+
+let broadcastAll = async receivers => {
+    await Promise.all(receivers.map(receiver => broadcast(receiver)));
+};
+
+listen(encryptedMsg => {
+    let decryptedMsg = decrypt(encryptedMsg, selfPrivkey);
+    if (decryptedMsg.broadcast) {
+
+    } else if (decryptedMsg.poll) {
+
+    }
+    assert(validSigners.contains(outerMsg.signer)); // i may not need this in some network configurations
+    let innerMsg = await unwrapSigned(outerMsg);
+    let text = await unwrapSigned(innerMsg);
 });
